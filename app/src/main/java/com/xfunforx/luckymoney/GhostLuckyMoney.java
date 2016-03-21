998b540b0dc8522da65e3bde77cbece68449ed92
yfcheng <yfcheng@mobvoi.com>
2016-03-21 19:47:41 +0800

    d

--- app/src/main/java/com/xfunforx/luckymoney/GhostLuckyMoney.java
+++ app/src/main/java/com/xfunforx/luckymoney/GhostLuckyMoney.java
@@ -12,6 +12,9 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.StringReader;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Random;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -32,8 +35,7 @@ public class GhostLuckyMoney implements IXposedHookLoadPackage {
 
     private void log(String tag, Object msg) {
 
-        ;
-//        XposedBridge.log(tag + " " + msg.toString());
+        XposedBridge.log(tag + " " + msg.toString());
     }
 
     private String readxml(String xmlstr) {
@@ -70,18 +72,19 @@ public class GhostLuckyMoney implements IXposedHookLoadPackage {
         try {if (!loadPackageParam.packageName.contains("tencent.mm")) {
             return;
         }
+
             //new message is coming here
             Class b = findClass("com.tencent.mm.booter.notification.b", loadPackageParam.classLoader);
             findAndHookMethod("com.tencent.mm.booter.notification.b", loadPackageParam.classLoader, "a", b, String.class, String.class, int.class, int.class, boolean.class, new XC_MethodHook() {
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 
-                    toggle = (Boolean) XposedHelpers.callStaticMethod(findClass("com.tencent.mm.g.a", loadPackageParam.classLoader), "pb");
+                    toggle = (Boolean) XposedHelpers.callStaticMethod(findClass("com.tencent.mm.g.a", loadPackageParam.classLoader), "pd");
                     // FIXME: 15/12/28 for wechat 6.3.8 pb
-                    log("toggle is:" , String.valueOf(toggle));
+                    log("toggle is:", String.valueOf(toggle) + "  " + param.args[3].toString());
                     if (toggle && param.args[3].toString().equals("436207665")) {
                         String nativeurl = readxml(param.args[2].toString());
-                        context = (Context) XposedHelpers.callStaticMethod(findClass("com.tencent.mm.sdk.platformtools.z", loadPackageParam.classLoader), "getContext");
+                        context = (Context) XposedHelpers.callStaticMethod(findClass("com.tencent.mm.sdk.platformtools.y", loadPackageParam.classLoader), "getContext");
                         Intent intent = new Intent();
                         intent.setClassName("com.tencent.mm", "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI");
                         int key;
@@ -100,14 +103,16 @@ public class GhostLuckyMoney implements IXposedHookLoadPackage {
                     }
                 }
             });
+
             // FIXME: 15/12/28 for wechat 6.3.8
             findAndHookMethod("com.tencent.mm.model.bc", loadPackageParam.classLoader, "ai", boolean.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
-
+                    log("com.tencent.mm.model.bc is:" , String.valueOf(param.args[0].toString()));
                     findAndHookMethod("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI", loadPackageParam.classLoader, "onCreate", Bundle.class, new XC_MethodReplacement() {
                         @Override
                         protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                            log("robot is:" , String.valueOf(robot));
                             if (robot){
                                 Activity activity = (Activity) methodHookParam.thisObject;
                                 activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON | WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -119,36 +124,37 @@ public class GhostLuckyMoney implements IXposedHookLoadPackage {
                     findAndHookMethod("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI", loadPackageParam.classLoader, "onCreate", Bundle.class, new XC_MethodHook() {
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            log("hasgothongbao is:" , String.valueOf(hasgothongbao));
                             if (!hasgothongbao) {
                                 hasgothongbao = true;
                                 XposedHelpers.callMethod(param.thisObject,"finish");
                             }
                         }
                     });
-                    Class j = findClass("com.tencent.mm.q.j", loadPackageParam.classLoader);
+                    Class j = findClass("com.tencent.mm.r.j", loadPackageParam.classLoader);
                     // button is ready for click here
                     findAndHookMethod("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI", loadPackageParam.classLoader, "e", int.class, int.class, String.class, j, new XC_MethodHook() {
                         @Override
                         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            log("robotd  is:" , String.valueOf(robot));
                             if (robot) {
                                 log("if robot so hook for auto click", "hongbao");
                                 Class receiveui = findClass("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI", loadPackageParam.classLoader);
-                                Button button = (Button) XposedHelpers.callStaticMethod(receiveui, "e", param.thisObject);
+                                Field buttonField = XposedHelpers.findField(receiveui, "ePT");
+
+//                                Button button = (Button) XposedHelpers.callStaticMethod(receiveui, "e", param.thisObject);
+                                Button button = (Button) XposedHelpers.getObjectField(param.thisObject, "ePT");
                                 log("the button text will change ,so get the text for use", button.getText());
-                                if (button.getText() != "" && button.isShown()) {
+                                if (button.isShown()) {
                                     hasgothongbao = false;
                                      Random rd = new Random();
                                    
-                                   Thread.sleep(rd.nextInt(1600));
+                                   Thread.sleep(rd.nextInt(800));
                                     button.performClick();
-<<<<<<< HEAD
                                     log("click", "get luckymoney button");
                                     // FIXME: 15/12/30 maybe fix the luckymoney is over,so maybe didnot close the window
                                 }else{
 //                                } else if (!button.isShown()) {
-=======
-                                } else if (!button.isShown()) {
->>>>>>> origin/master
                                     XposedHelpers.callMethod(param.thisObject, "finish");
                                     Toast.makeText(context, "(^O^)", Toast.LENGTH_SHORT).show();
                                 }
@@ -156,6 +162,34 @@ public class GhostLuckyMoney implements IXposedHookLoadPackage {
                             }
                         }
                     });
+
+                    findAndHookMethod("com.tencent.mm.plugin.sns.lucky.ui.SnsLuckyMoneyWantSeePhotoUI", loadPackageParam.classLoader, "onActivityResult", int.class, int.class,Intent.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.args[1] = Activity.RESULT_OK;
+                            super.beforeHookedMethod(param);
+                        }
+
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        }
+                    });
+
+                    findAndHookMethod("com.tencent.mm.plugin.sns.lucky.ui.SnsLuckyMoneyNewYearMedalUI", loadPackageParam.classLoader, "onCreate",Bundle.class, new XC_MethodHook() {
+
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            Toast.makeText((Context) param.thisObject, "点击空白处即可见证奇迹", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    findAndHookMethod("com.tencent.mm.plugin.sns.lucky.ui.SnsLuckyMoneyNewYearSendUI", loadPackageParam.classLoader, "onCreate",Bundle.class, new XC_MethodHook() {
+
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            Toast.makeText((Context) param.thisObject, "点击空白处即可见证奇迹", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
                 }
             });
         } catch (Exception e) {
